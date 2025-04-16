51c7098361a2568f441a8895a861ba8582212b6d
Kemeow815 <kemiao@kmblog.icu>
2025-04-16 22:33:18 +0800

    feat: add shuoshuo link back to header

--- components/header.tsx
+++ components/header.tsx
@@ -10,7 +10,7 @@ const links = [
   { name: '关于', path: '/about' },
   { name: '博客', path: '/blog' },
   { name: '音乐', path: 'https://music.kemiao.online', target: '_blank' },
-  // { name: '说说', path: 'https://shuoshuo.kemiao.online' },
+  { name: '说说', path: 'https://shuoshuo.kemiao.online' },
 ]
 
 const Header = (): JSX.Element => {
@@ -27,16 +27,16 @@ const Header = (): JSX.Element => {
           </Link>
           <nav className={styles.nav}>
             <ol className={styles.links}>
-              {links.map(({ name, path }) => {
-                // if (target === '_blank') {
-                //   return (
-                //     <li key={path} className={pathname === path ? styles.linkActive : styles.link}>
-                //       <a href={path} target="_blank" rel="noopener noreferrer">
-                //         {name}
-                //       </a>
-                //     </li>
-                //   )
-                // }
+              {links.map(({ name, path, target }) => {
+                if (target === '_blank') {
+                  return (
+                    <li key={path} className={pathname === path ? styles.linkActive : styles.link}>
+                      <a href={path} target="_blank" rel="noopener noreferrer">
+                        {name}
+                      </a>
+                    </li>
+                  )
+                }
                 return (
                   <li key={path} className={pathname === path ? styles.linkActive : styles.link}>
                     <Link href={path}>
